cd7a2fea938716052ef5e8ae4b28d3904a5c43da
Daniel Lesperance <danlesperance12@gmail.com>
2021-07-24 15:08:51 -0400

    Update Formula1_data_pull.py
    Finished update.  yrs, and the first html_to_df_csvDump were used to pull all data for 1950 to current day.  The current iteration allows me to pull data just for this year and any years I may miss in the future.

--- Python/Formula1_data_pull.py
+++ Python/Formula1_data_pull.py
@@ -1,23 +1,19 @@
-import urllib.request as urllib
-from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
+from sqlalchemy import create_engine
 
 
-this_year = datetime.now().year
-# URL address creation
-formula1_page = "https://www.formula1.com/en/results.html/2021/races.html"
-def url_pull(page):
+
+def url_pull(F1page):
     # Start scraping from html
-    page = urllib.urlopen(page)
-    #Run without visual browser pop-up with chrome
     options = Options()
+    # Run without visual browser pop-up with chrome
     options.headless = True
     driver = webdriver.Chrome(chrome_options=options)
     # Obtain the years for Formula1 data
-    years=[]
-    driver.get(page)
+    years = []
+    driver.get(F1page)
     elem = driver.find_elements_by_xpath('//li[@class="resultsarchive-filter-item"]')
     for a in elem:
         content = driver.execute_script('return arguments[0].textContent;',a)
@@ -26,21 +22,21 @@ def url_pull(page):
             int(content)
         except ValueError:
             break
-        if content != str(this_year):
-            years.append(content)
-        else:
-            pass
+        years.append(content)
     driver.quit()
     return years
 
 # Pull each table from the website for each given year, the different categories are different tables that can be pulled.
-def html_to_df(Formula1Years):
+def html_to_df_csvDump(Formula1Years,sqlCon):
     html_start ='https://www.formula1.com/en/results.html'
     categories = ['races','drivers','team','fastest-laps']
-    for year in Formula1Years:
-        for cat in categories:
+    for cat in categories:
+        for year in Formula1Years:
             new_html = html_start+'/'+year+'/'+cat+'.html'
-            table = pd.read_html(new_html)
+            try:
+                table = pd.read_html(new_html)
+            except ValueError:
+                break
             df = table[0]
             df['year'] = pd.Series([year for x in range(len(df.index))])
             df.dropna(how='all', axis=1, inplace=True)
@@ -48,20 +44,28 @@ def html_to_df(Formula1Years):
                 df[['FirstName','LastIdentifier']] = df['Driver'].str.split('  ',1,expand=True)
                 df[['LastName','Identifier']] = df['LastIdentifier'].str.split('  ',1,expand=True)
                 df=df.drop(['Driver','LastIdentifier'],axis=1)
+                if cat == 'drivers':
+                    df.loc[(df['Pos'] == 'DQ'),'Pos'] = '0'
+            elif cat == 'team':
+                df.loc[(df['Pos'] == 'EX'), 'Pos'] = '0'
             elif cat == 'races':
                 df[['FirstName', 'LastIdentifier']] = df['Winner'].str.split('  ', 1, expand=True)
                 df[['LastName', 'WinningIdentifier']] = df['LastIdentifier'].str.split('  ', 1, expand=True)
                 df = df.drop(['Winner', 'LastIdentifier'], axis=1)
-            df.to_csv('Formula1_' + cat + '_' + year + '.csv', index=False)
-        break
+            df.to_sql(con=sqlCon, name=cat, if_exists='append')
+            #if year == '2021':
+                #df.to_csv('Formula1_' + cat + '.csv', index=False)
+            #else:
+                #df.to_csv('Formula1_' + cat + '.csv', index=False,mode='a',header=False)
 
 
 
-
-
-
-
-
-
-
-
+### Main
+# URL address creation
+formula1_page = "https://www.formula1.com/en/results.html/2021/races.html"
+sqlEngine = create_engine('mysql+pymysql://root:B1ology!@localhost:3306/formula1_data_dl')
+#yrs = url_pull(formula1_page)
+#html_to_df_csvDump(yrs,sqlEngine)
+yr=['2021']
+html_to_df_csvDump(yr,sqlEngine)
+sqlEngine.dispose()
